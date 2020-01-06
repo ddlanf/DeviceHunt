742dd9a1714e4a348f89a6f43c018e21412addfe
Daniel Laufenberg <danlanfen26@gmail.com>
2020-01-05 23:34:03 -1000

    back to normal

--- index.js
+++ index.js
@@ -53,16 +53,11 @@ function phoneEffect(){
   setTimeout(function(){
   $( '.phone-image' ).css('animation', 'none');
   if($(window).width() < 600){ 
-    $( '.image-surrounder' ).css('transform', 'rotate(360deg) scale(2.8, 2.8) translateY(-31px)');
-    $( '.image-surrounder' ).css('-ms-transform', 'rotate(360deg) scale(2.8, 2.8) translateY(-31px)');
-    $( '.image-surrounder' ).css('-webkit-transform', 'rotate(360deg) scale(2.8, 2.8) translateY(-31px)');
-    $( '.image-surrounder' ).css('-moz-transform', 'rotate(360deg) scale(2.8, 2.8) translateY(-31px)');
-    $( '.image-surrounder' ).css('-o-transform', 'rotate(360deg) scale(2.8, 2.8) translateY(-31px)');
-   /*$( '.phone-image' ).css('transform', 'rotate(360deg) scale(2.8, 2.8) translateY(-31px)');
+    $( '.phone-image' ).css('transform', 'rotate(360deg) scale(2.8, 2.8) translateY(-31px)');
     $( '.phone-image' ).css('-ms-transform', 'rotate(360deg) scale(2.8, 2.8) translateY(-31px)');
     $( '.phone-image' ).css('-webkit-transform', 'rotate(360deg) scale(2.8, 2.8) translateY(-31px)');
     $( '.phone-image' ).css('-moz-transform', 'rotate(360deg) scale(2.8, 2.8) translateY(-31px)');
-    $( '.phone-image' ).css('-o-transform', 'rotate(360deg) scale(2.8, 2.8) translateY(-31px)');*/
+    $( '.phone-image' ).css('-o-transform', 'rotate(360deg) scale(2.8, 2.8) translateY(-31px)');
   }
   else if($(window).width() > 600 && $(window).width() < 1000 ){
     $( '.phone-image' ).css('transform', 'rotate360deg) scale(4.2, 4.2) translateY(15px)'); 
